7cc7bbad3be5d35949bd3262938dfc2fbb0e578e
Diegoferreiradev <diegoferreiradev@gmail.com>
2024-04-14 21:43:11 -0300

    first commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudBlazor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudBlazor.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; } = null!;
+    }
+}
